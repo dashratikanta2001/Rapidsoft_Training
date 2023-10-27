5165cc6c95615004fd6ea3ead472c47ac2af2788
Ratikanata <69645865+dashratikanta2001@users.noreply.github.com>
2023-10-27 15:00:36 +0530

    training

--- Hibernate Rapidsoft/Assignment/MCQQuizGenerator/src/main/java/com/main/App.java
+++ Hibernate Rapidsoft/Assignment/MCQQuizGenerator/src/main/java/com/main/App.java
@@ -26,3 +26,55 @@ public class App
         factory.close();
     }
 }
+
+//STEP-1
+//The menu will come where the user will enter the menu number to perform the respective action.
+//
+//STEP-2  Add User
+//New user will provide their name, phoneNo, emailId,and password to register.
+//After successfull registration it will show the userId of that user.
+//
+//STEP-3 Start Quiz
+//The user will enter their respective userId to start the quiz.
+//if the userId is not present it provide that user is not available.
+//
+//STEP-3.1
+//The question and respective options are displayed.
+//User has to enter the respective option serial number to save the answer.
+//if the user will provide outside of the option number then it will ask to provide the correct option number.
+//
+//and so on....
+//
+//STEP-4 Show mark by Id
+//The user will enter their id to show their individual mark.
+//If the user is not given the Quiz then it shows their obtained mark as 0.
+//
+//if the userId is not present it provide that user is not available.
+//
+//STEP-5 Show All Marks
+//It will displayed all users obtained mark and total mark.
+//
+//STEP-6 Add Question
+//
+//First it will ask to enter their userId and password to authenticate.
+//Then it will show another menu to perform the Operation like: Set Question, Set Option and Set Answer.
+//the user has to enter the respective SL no.
+//
+//STEP-6.1 Set Question
+//User has to enter the question
+//after that it shows the question id.
+//
+//STEP-6.2 Set Option
+//It will ask to enter the question id
+//if the ID is present then it allows to enter the option
+//After that it ask to enter another option 
+//If press Y then it ask to enter another option else it shows the menu
+//
+//STEP-6.3 Set Answer
+//It ask to enter the question id
+//if the ans is set for that id then it shows that answer already set
+//else it shows the avalable options for that question
+//here user have to enter the serial number of the option to set as answer.
+//
+
+
--- Hibernate Rapidsoft/Assignment/MCQQuizGenerator/src/main/java/com/userMarks/UserMark.java
+++ Hibernate Rapidsoft/Assignment/MCQQuizGenerator/src/main/java/com/userMarks/UserMark.java
@@ -1,6 +1,8 @@
 package com.userMarks;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -20,7 +22,7 @@ public class UserMark {
 
 		String ansQuery = "from QuestionAnswer";
 		List<QuestionAnswer> ansList = s.createQuery(ansQuery, QuestionAnswer.class).getResultList();
-		int totalMark=ansList.size();
+		int totalMark = ansList.size();
 
 		String userAnsQuery = "from UserInput";
 		List<UserInput> userAnsList = s.createQuery(userAnsQuery, UserInput.class).getResultList();
@@ -39,14 +41,21 @@ public class UserMark {
 			}
 		}
 
-		System.out.println("Name : " + s.get(User.class, userId).getName()) ;
+		System.out.println("Name : " + s.get(User.class, userId).getName());
 		System.out.println("Mark Obtained : " + mark);
-		System.out.println("Total Mark : "+totalMark);
+		System.out.println("Total Mark : " + totalMark);
+		System.out.println("Grade: '" + gradeCalculator(mark, totalMark)+"'");
+		
+//		quizAttended();
 
 	}
 
 	public void getAllMarks() {
 		// TODO Auto-generated method stub
+		
+		int studentPassed=0;
+		int studentFailed=0;
+		Map<Integer,Integer> attendedStudent = new HashMap<>();
 
 		SessionFactory factory = App.factory;
 		Session s = factory.openSession();
@@ -57,7 +66,7 @@ public class UserMark {
 		String ansQuery = "from QuestionAnswer";
 		List<QuestionAnswer> ansList = s.createQuery(ansQuery, QuestionAnswer.class).getResultList();
 
-		int totalMark=ansList.size();
+		int totalMark = ansList.size();
 		String userAnsQuery = "from UserInput";
 		List<UserInput> userAnsList = s.createQuery(userAnsQuery, UserInput.class).getResultList();
 
@@ -65,6 +74,8 @@ public class UserMark {
 			int mark = 0;
 
 			for (UserInput userInput : userAnsList) {
+				
+				attendedStudent.put(userInput.getUser().getUser_id(), userInput.getUser().getUser_id());
 
 				for (QuestionAnswer questionAnswer : ansList) {
 
@@ -77,14 +88,73 @@ public class UserMark {
 					}
 				}
 			}
-//			System.out.println("Name : " + user.getName() + "\nMark Obtained: " + mark);
 			
-			System.out.println("Name : " +  user.getName());
+			String grade=gradeCalculator(mark, totalMark);
+			
+			if(!grade.equals("D"))
+			{
+				studentPassed++;
+			}
+			else
+			{
+				studentFailed++;
+			}
+			
+			System.out.println("Name : " + user.getName());
 			System.out.println("Mark Obtained : " + mark);
-			System.out.println("Total Mark : "+totalMark);
+			System.out.println("Total Mark : " + totalMark);
+			System.out.println("Grade: '" + grade+"'");
 			System.out.println("-------------------------------");
 
 		}
+		
+		System.out.println("Total User available : "+userList.size());
+		System.out.println("Total User Attended the Quiz : "+attendedStudent.size());
+		System.out.println("Total User Passed: "+studentPassed);
+		System.out.println("Total User Failed: "+studentFailed);
 
 	}
+
+	private String gradeCalculator(int obtained, int total) {
+		// TODO Auto-generated method stub
+		String grade = "";
+		int mark = (obtained * 100) / total;
+
+		switch (mark / 10) {
+		case 10:
+		case 9:
+			return "O";
+		case 8:
+			return "A1";
+		case 7:
+			return "A2";
+		case 6:
+			return "B1";
+		case 5:
+			return "B2";
+		case 4:
+			return "C";
+		default:
+			return "D";
+		}
+	}
+	
+//	private int quizAttended() {
+//		// TODO Auto-generated method stub
+//		SessionFactory factory = App.factory;
+//		Session s = factory.openSession();
+//
+//		String userQuery = "from UserInput";
+//		List<UserInput> userList = s.createQuery(userQuery, UserInput.class).getResultList();
+//		
+//		Map<Integer,Integer> hs = new HashMap<>();
+//		
+//		
+//		for (UserInput userInput : userList) {
+//			hs.put(userInput.getUser().getUser_id(), userInput.getUser().getUser_id());
+//		}
+//		
+//		return hs.size();
+//	}
+
 }
