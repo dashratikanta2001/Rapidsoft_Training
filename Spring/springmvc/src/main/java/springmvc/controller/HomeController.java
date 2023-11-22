1ba20dae98b91d141a6ac1270ffcf18d99fea66a
Ratikanata <69645865+dashratikanta2001@users.noreply.github.com>
2023-11-22 18:33:38 +0530

    training

---
+++ Spring/springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,24 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/home")
+	public String home()
+	{
+		
+		System.out.println("This is home url");
+		return "index";
+	}
+	
+	public String about()
+	{
+		System.out.println("About page ");
+		return "about";
+	}
+	
+}
