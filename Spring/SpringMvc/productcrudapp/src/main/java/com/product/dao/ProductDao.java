67417a3c79945b7d101906813a6c23caa6a28201
Ratikanata <69645865+dashratikanta2001@users.noreply.github.com>
2023-11-27 18:38:48 +0530

    training

---
+++ Spring/SpringMvc/productcrudapp/src/main/java/com/product/dao/ProductDao.java
@@ -0,0 +1,45 @@
+package com.product.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import com.product.entity.Product;
+
+@Component
+public class ProductDao {
+
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	// create
+	@Transactional
+	public void createProduct(Product product) {
+		this.hibernateTemplate.save(product);
+	}
+
+	// get all product
+
+	public List<Product> getProducts() {
+		List<Product> products = this.hibernateTemplate.loadAll(Product.class);
+
+		return products;
+	}
+
+	// get the single product
+	public Product getProduct(int pid) {
+		return this.hibernateTemplate.load(Product.class, pid);
+	}
+
+	// delete the single product
+	@Transactional
+	public void deleteProduct(int pid) {
+		Product product = this.hibernateTemplate.load(Product.class, pid);
+		this.hibernateTemplate.delete(product);
+	}
+
+}
